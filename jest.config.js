e35722d3e443dcdea16c91fce17fd7a11d97a942
ken0gul <ogulcanken@gmail.com>
2024-08-02 21:20:15 +0300

    removed client-wise-ui folder

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
-  },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-};
